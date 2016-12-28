c620f01e3c20b62d82d784203bbb460580f04f33
priyasivakumar <priya241297@gmail.com>
2016-12-27 20:38:25 -0800

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("\nEnter the value:");
+    scanf("%d",&a);
+    if(a%2==0)
+        printf("even value");
+    else
+        printf("odd value");
+  getch();
+}
